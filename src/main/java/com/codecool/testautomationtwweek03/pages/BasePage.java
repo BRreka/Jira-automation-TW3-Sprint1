8416cf6135eb51233ad34d1f8efe717f296ea810
Bencze Réka <benczereka9@gmail.com>
2023-06-20 21:16:06 +0200

    add properties field to base page

--- src/main/java/com/codecool/testautomationtwweek03/pages/BasePage.java
+++ src/main/java/com/codecool/testautomationtwweek03/pages/BasePage.java
@@ -1,13 +1,16 @@
 package com.codecool.testautomationtwweek03.pages;
 
+import com.codecool.testautomationtwweek03.init.PropertiesUtil;
 import com.codecool.testautomationtwweek03.init.WebdriverUtil;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.*;
 
 import java.time.Duration;
+import java.util.Properties;
 
 public abstract class BasePage {
     protected final WebDriver driver = WebdriverUtil.getInstance();
+    protected final Properties properties = PropertiesUtil.getInstance();
 
     public BasePage() {
         PageFactory.initElements(driver,this);
