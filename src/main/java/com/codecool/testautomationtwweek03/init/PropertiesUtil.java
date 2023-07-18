e121aca8327421a9677ecbe477f98a27ac9a7fdd
Bencze Réka <benczereka9@gmail.com>
2023-07-18 13:55:21 +0200

    set config.properties path into a variable in PropertiesUtil

--- src/main/java/com/codecool/testautomationtwweek03/init/PropertiesUtil.java
+++ src/main/java/com/codecool/testautomationtwweek03/init/PropertiesUtil.java
@@ -1,14 +1,18 @@
 package com.codecool.testautomationtwweek03.init;
 
 import java.io.*;
-import java.util.Properties;
+import java.util.*;
 
 public class PropertiesUtil {
+    private static final String CONFIG_PROPERTIES_PATH = null;
     private static volatile Properties properties;
 
     private PropertiesUtil() {}
 
     public static Properties getInstance() {
+        if(CONFIG_PROPERTIES_PATH == null) {
+            throw new IllegalArgumentException("Please provide config path");
+        }
         Properties prop = properties;
         if(prop == null) {
             synchronized (PropertiesUtil.class) {
@@ -16,7 +20,7 @@ public class PropertiesUtil {
                 if(prop == null) {
                     prop = new Properties();
                     try {
-                        FileInputStream ip = new FileInputStream("config.properties path");
+                        FileInputStream ip = new FileInputStream(CONFIG_PROPERTIES_PATH);
                         prop.load(ip);
                     } catch (IOException e) {
                         e.printStackTrace();
